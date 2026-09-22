fee3b557f5a36c2361d9c81e2c86c6a3459f266a
Fabien Lecouve <fabien.lecouve@hotmail.com>
2026-09-22 16:09:39 +0200

    fix: handle dynamic font colors in quick messages

--- resources/js/pages/quick-messages.js
+++ resources/js/pages/quick-messages.js
@@ -50,6 +50,7 @@ window.quickMessages = function (projects, languagesByProject, data) {
         },
 
         getCategoryStyle(category) {
+            console.log(category.label, category.background_color);
             return {
                 color: category.font_color,
                 backgroundColor: category.background_color,
